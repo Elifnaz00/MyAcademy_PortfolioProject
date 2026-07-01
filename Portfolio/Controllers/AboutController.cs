1f63beda780ce203d5932a0d44e82f7f96946279
Elifnaz00 <demirelifnaz9@gmail.com>
2026-07-01 22:34:21 +0300

    Proje dosyası ekle.

---
+++ Portfolio/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public AboutController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var about = _appDbContext.Abouts.FirstOrDefault();
+            return View(about);
+        }
+    }
+}
